dc95f7cd653cdab261a90c26ff522628d3336710
EtoZheVladoZ <54363763+EtoZheVladoZ@users.noreply.github.com>
2019-09-01 13:59:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,15 +23,19 @@ async def on_member_remove(member):
 async def on_voice_state_update(member, before, after):
     cname = f"Канал {str(member.name)}"
     main_channel = member.guild.get_channel(615541017768099840)
+    f = open('id.txt', 'r+')
     if after.channel == main_channel:
-        await  member.guild.create_voice_channel(name = cname, user_limit = 2, category = after.channel.category)
+        chl = await  member.guild.create_voice_channel(name = cname, user_limit = 2, category = after.channel.category)
         channel = discord.utils.get(member.guild.voice_channels, name = cname)
         await member.move_to(channel)
-        #overwrite = discord.PermissionOverwrite(manage_channels = True)
-        #await channel.set_permissions(member, overwrite = overwrite)
+        overwrite = discord.PermissionOverwrite(manage_channels = True)
+        await channel.set_permissions(member, overwrite = overwrite)
+        f.write(str(chl.id))
     if before.channel is not None:
-        if before.channel.name == cname:
+        id_chl = int(f.read())
+        if before.channel.id == id_chl and before.channel.members == []:
             await before.channel.delete()
+            f.close
     
 token = os.environ.get('BOT_TOKEN')
 Client.run(str(token))
