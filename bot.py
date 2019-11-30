3a7ec7ca0042fb5cda77df4af7192ac7474d3e8e
EtoZheVladoZ <54363763+EtoZheVladoZ@users.noreply.github.com>
2019-11-30 18:08:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 import time
 
-Client = commands.Bot(command_prefix='$')
+Client = commands.Bot(command_prefix='+')
 
 @Client.event
 async def on_ready():
